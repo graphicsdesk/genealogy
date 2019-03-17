c14d9966e728944b496b829c6275b750ce2776f1
Jason Kao <jason.kao@columbia.edu>
2019-03-17 13:57:14 -0400

    created scroll template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import archieml from 'archieml';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Graphic from './Graphic';
+import copy from './copy';
+
+const steps = archieml.load(copy).steps;
+
+const App = () => (
+  <div>
+    <Graphic steps={steps} />
+  </div>
+);
 
 export default App;
