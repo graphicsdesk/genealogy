dd135411cc1fb12c4d13b35e42d20bcd774a7225
Jason Kao <jason.kao@columbia.edu>
2019-03-17 13:59:44 -0400

    created copy -> graphic steps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import archieml from 'archieml';
+
+import Graphic from './Graphic';
+import copy from './copy';
+
+const steps = archieml.load(copy).steps;
 
 const App = () => (
   <div>
+    <Graphic steps={steps} />
   </div>
 );
 
