488c90030aebc52e8b5ebe533d5661fb94ee391e
Jason Kao <jason.kao@columbia.edu>
2019-03-18 21:09:07 -0400

    Updates

---
+++ precache-manifest.19ef0d48eed331896a0fa46a21d5d524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c29e8623260d867b2b2dc8d4e896855d",
+    "url": "/genealogy/static/media/portraits.c29e8623.jpg"
+  },
+  {
+    "revision": "d04420a296f77e88649a1512dd01cc64",
+    "url": "/genealogy/static/media/ancestral-map.d04420a2.jpg"
+  },
+  {
+    "revision": "d6c618738febd2a884bf",
+    "url": "/genealogy/static/js/runtime~main.ac4a6a33.js"
+  },
+  {
+    "revision": "4e0a9e3fd0d30e4a82f1",
+    "url": "/genealogy/static/js/main.44cdee03.chunk.js"
+  },
+  {
+    "revision": "a4afffb8556ae6d2d983",
+    "url": "/genealogy/static/js/2.d1d1932c.chunk.js"
+  },
+  {
+    "revision": "76930e1ea9675e498a0dcc1d954e092d",
+    "url": "/genealogy/index.html"
+  }
+];
