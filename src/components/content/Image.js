c5e956019204a3bb7130f47b38305551e7987f88
Jason Kao <jason.kao@columbia.edu>
2019-03-25 09:40:13 -0400

    prettier

--- src/components/content/Image.js
+++ src/components/content/Image.js
@@ -6,9 +6,9 @@ const getImage = name => {
     case 'PortraitOfYuanTungLi.jpg':
       return require('./images/PortraitOfYuanTungLi.jpg');
     case 'FairbankLetter.jpg':
-    return require('./images/FairbankLetter.jpg');
-  case 'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg':
-    return require('./images/MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg');
+      return require('./images/FairbankLetter.jpg');
+    case 'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg':
+      return require('./images/MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg');
     default:
       console.error(`Could not get image with name ${name} in getImage().`);
       return null;
