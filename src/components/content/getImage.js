3534594720a3e8e30cc83b1063417fd6511750d4
Jason Kao <jason.kao@columbia.edu>
2019-03-26 11:18:40 -0400

    deleted all assets

--- src/components/content/getImage.js
+++ src/components/content/getImage.js
@@ -1,8 +1,12 @@
 const imageUrls = {
-  'FairbankLetter.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/IZQVNJFKEZHJ5I6C6GFY244T2Y.jpg',
-  'PortraitOfYuanTungLi.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/Y3HDZRHGVBDR7DTZ27LM53QOAY.jpg',
-  'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/GMPS4G7JSNGHFJYWYBOMUNEN3A.jpg',
-  'Cover_Illustration.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KJCRXDTAK5BBRFLGDKJUPJ6RCY.jpg',
+  'FairbankLetter.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/IZQVNJFKEZHJ5I6C6GFY244T2Y.jpg',
+  'PortraitOfYuanTungLi.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/Y3HDZRHGVBDR7DTZ27LM53QOAY.jpg',
+  'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/GMPS4G7JSNGHFJYWYBOMUNEN3A.jpg',
+  'Cover_Illustration.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KJCRXDTAK5BBRFLGDKJUPJ6RCY.jpg',
 };
 
 export default name => imageUrls[name];
--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const imageUrls = {
-  'poem.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FGHZIHGLURDEHG3XUPBVMHZG2Q.jpg',
-  'women.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/PWPTOPFN7VA4PC5SZ7D5FI6PGU.jpg',
-  'ancestral-map.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FST5QKGRDNGRBDG5A2E2LXAC6U.jpg',
-  'portraits.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/YS7N6SV4SFEL5I63PQTXS4GJCI.jpg',
+  'poem.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FGHZIHGLURDEHG3XUPBVMHZG2Q.jpg',
+  'women.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/PWPTOPFN7VA4PC5SZ7D5FI6PGU.jpg',
+  'ancestral-map.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FST5QKGRDNGRBDG5A2E2LXAC6U.jpg',
+  'portraits.jpg':
+    'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/YS7N6SV4SFEL5I63PQTXS4GJCI.jpg',
 };
 
 const Image = ({ dims, name, leftSide = false }) => {
