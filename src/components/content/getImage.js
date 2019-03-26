8cf7cace7a9a0247b13b39f97b235239c386d7cb
Jason Kao <jason.kao@columbia.edu>
2019-03-26 11:13:02 -0400

    replaced font and image urls with anglerfish and s3

--- src/components/content/getImage.js
+++ src/components/content/getImage.js
@@ -1,15 +1,8 @@
-export default name => {
-  switch (name) {
-    case 'PortraitOfYuanTungLi.jpg':
-      return require('./images/PortraitOfYuanTungLi.jpg');
-    case 'FairbankLetter.jpg':
-      return require('./images/FairbankLetter.jpg');
-    case 'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg':
-      return require('./images/MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg');
-    case 'Cover_Illustration.jpg':
-      return require('./images/Cover_Illustration.jpg');
-    default:
-      console.error(`Could not get image with name ${name} in getImage().`);
-      return null;
-  }
+const imageUrls = {
+  'FairbankLetter.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/IZQVNJFKEZHJ5I6C6GFY244T2Y.jpg',
+  'PortraitOfYuanTungLi.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/Y3HDZRHGVBDR7DTZ27LM53QOAY.jpg',
+  'MicrofilmedforGenealogicalSocietyOfSaltLakeCity.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/GMPS4G7JSNGHFJYWYBOMUNEN3A.jpg',
+  'Cover_Illustration.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KJCRXDTAK5BBRFLGDKJUPJ6RCY.jpg',
 };
+
+export default name => imageUrls[name];
--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const IMGDIR = './imageAssets';
+const imageUrls = {
+  'poem.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FGHZIHGLURDEHG3XUPBVMHZG2Q.jpg',
+  'women.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/PWPTOPFN7VA4PC5SZ7D5FI6PGU.jpg',
+  'ancestral-map.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FST5QKGRDNGRBDG5A2E2LXAC6U.jpg',
+  'portraits.jpg': 'https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/YS7N6SV4SFEL5I63PQTXS4GJCI.jpg',
+};
 
 const Image = ({ dims, name, leftSide = false }) => {
   const { leftX, rightX, y, width, height } = dims;
@@ -11,7 +16,7 @@ const Image = ({ dims, name, leftSide = false }) => {
       y={y}
       width={width}
       height={height}
-      xlinkHref={require(`${IMGDIR}/${name}`)}
+      xlinkHref={imageUrls[name]}
     />
   );
 };
